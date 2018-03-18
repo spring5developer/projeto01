5639724a9946e0f6e7204b0abee91983aaf7afb4
Jhonathan <jhonathandeveloper@gmail.com>
2018-03-18 10:21:53 -0300

    first commit

---
+++ src/main/java/jhonathandeveloper/projeto01/entity/User.java
@@ -0,0 +1,50 @@
+package jhonathandeveloper.projeto01.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String email;
+	
+	@ManyToMany
+	private Set<Role> roles;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+}
+	
